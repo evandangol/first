e05fa6b0362a89518b994c01c71d82072ce0e382
evandangol <smartevu@gmail.com>
2021-12-07 12:46:28 +0545

    sfds

---
+++ JString.java
@@ -0,0 +1,38 @@
+public class JString {
+    public static void main(String[] args) {
+        // String name="kathmandu";
+        // int len=name.length();
+        // System.out.println(len);
+
+        // System.out.println(name.toUpperCase());
+
+
+
+
+        // String name1="POKHARA";
+        // String name2=name1.toLowerCase();
+        // System.out.println(name2);
+        // //System.out.println(name1.toLowerCase());
+
+      //  String address="katmahamndu";
+
+       // System.out.println(address.indexOf("a"));
+
+    //    String firstname="Ram";
+    //    String lastname="Sharma";
+
+    //    String fullname=firstname+" "+lastname;
+
+    //    System.out.println(fullname);
+
+    //String txt = "We are the so-called \"Vikings\" from the north.";
+
+    //System.out.println(txt);
+    // String xyz="Fill in the missing part to create\n a greeting variable of type String and assign it the value Hello. \nFill in the missing part to create a greeting variable of type String and assign it the value Hello. \nnepal";
+    // System.out.println(xyz);
+    // String x="name ram  \t\taddress khusibu";
+    // System.out.println(x);
+    String name="Elina";
+    System.out.println(name);
+    }
+}
